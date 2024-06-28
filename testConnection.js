c2f7184dae3108aef74c163850e872a1ae07ac4a
emimelo <emillymelo3@gmail.com>
2024-06-28 11:56:02 -0300

    Fix: environment variable for MongoDB URI

--- testConnection.js
+++ testConnection.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-async function main() {
-    try{
+async function testConnection() {
+    try {
         mongoose.set('strictQuery', true);
-
+        
         await mongoose.connect(process.env.MONGODB_CONNECT_URL);
 
         console.log('MongoDB connected');
-    }
-    catch(error){
-        console.log(error);
+        mongoose.connection.close();
+    } catch (error) {
+        console.error('MongoDB connection error:', error.message);
+        process.exit(1); 
     }
 }
 
-module.exports = main
+testConnection();
